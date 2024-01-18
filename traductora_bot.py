349149ce8b50be7244481d13aec292bf8141d72f
Wisdoms <medianhq@gmail.com>
2024-01-19 00:31:49 +0200

    اة

--- traductora_bot.py
+++ traductora_bot.py
@@ -859,6 +859,8 @@ def handle_trans_reply(message, to_lang, flag_msg, from_lang, from_locale, to_lo
 
 
 def translate_voice(message:telebot.types.Message, from_lang, to_lang, from_locale, to_locale):
+    bot.reply_to(message, "`Sorry! Bot currently doesn't fully support voice-note translation on the host site (repl.it).`",parse_mode="Markdown")
+    return # currently repl.it can't run the speech sdk
 
     voice_note_file_path = bot.get_file(message.voice.file_id).file_path
     print('voice_note_file_path:', voice_note_file_path)
